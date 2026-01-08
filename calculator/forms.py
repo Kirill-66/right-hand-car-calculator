ffce74c334af4d0f73e206213faf4e7bc6466ecd
Kirill-66 <kirysha.kurakin@mail.ru>
2026-01-08 14:39:26 +0500

    Добавлен интерфейс для калькулятора расходов
    небольшие правки + интерфейс калькулятора авто

--- calculator/forms.py
+++ calculator/forms.py
@@ -4,34 +4,90 @@ from .models import OwnershipCalculation, RightHandCar
 class CalculationForm(forms.ModelForm):
     """Форма для расчета стоимости владения"""
     
-    # Поле для выбора автомобиля с группировкой по маркам
+    # Автомобиль с кастомным виджетом
     car = forms.ModelChoiceField(
         queryset=RightHandCar.objects.all(),
         label="Выберите автомобиль",
-        widget=forms.Select(attrs={'class': 'form-control'})
+        widget=forms.Select(attrs={
+            'class': 'form-control-custom select',
+            'placeholder': '-- Выберите модель --'
+        })
+    )
+    
+    # Цена в Японии
+    purchase_price_jpy = forms.IntegerField(
+        label="Цена в Японии (¥)",
+        min_value=100000,
+        max_value=5000000,
+        initial=500000,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control-custom',
+            'placeholder': '500000'
+        })
+    )
+    
+    # Годовой пробег
+    annual_mileage = forms.IntegerField(
+        label="Годовой пробег (км)",
+        min_value=5000,
+        max_value=50000,
+        initial=15000,
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control-custom',
+            'placeholder': '15000'
+        })
+    )
+    
+    # Срок владения
+    OWNERSHIP_CHOICES = [
+        (1, '1 год'),
+        (2, '2 года'),
+        (3, '3 года'),
+        (4, '4 года'),
+        (5, '5 лет'),
+    ]
+    
+    ownership_years = forms.ChoiceField(
+        label="Срок владения",
+        choices=OWNERSHIP_CHOICES,
+        initial=3,
+        widget=forms.Select(attrs={
+            'class': 'form-control-custom select'
+        })
+    )
+    
+    # Регион
+    REGION_CHOICES = [
+        ('moscow', 'Москва'),
+        ('spb', 'Санкт-Петербург'),
+        ('vladivostok', 'Владивосток'),
+        ('ekb', 'Екатеринбург'),
+        ('other', 'Другой регион'),
+    ]
+    
+    region = forms.ChoiceField(
+        label="Ваш регион",
+        choices=REGION_CHOICES,
+        initial='moscow',
+        widget=forms.Select(attrs={
+            'class': 'form-control-custom select'
+        })
     )
     
     class Meta:
         model = OwnershipCalculation
         fields = ['car', 'purchase_price_jpy', 'annual_mileage', 'ownership_years', 'region']
-        widgets = {
-            'purchase_price_jpy': forms.NumberInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Пример: 500000'
-            }),
-            'annual_mileage': forms.NumberInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Пример: 15000'
-            }),
-            'ownership_years': forms.Select(attrs={'class': 'form-control'}),
-            'region': forms.Select(attrs={'class': 'form-control'}),
-        }
+        
+        # Удаляем дублирующиеся виджеты, т.к. переопределили поля
+        widgets = {}
+        
         labels = {
             'purchase_price_jpy': 'Цена в Японии (иены)',
             'annual_mileage': 'Годовой пробег (км)',
             'ownership_years': 'Срок владения',
             'region': 'Ваш регион',
         }
+        
         help_texts = {
             'purchase_price_jpy': 'Средняя цена на аукционе в Японии',
             'annual_mileage': 'Сколько примерно проезжаете за год',
--- calculator/views.py
+++ calculator/views.py
@@ -1,38 +1,65 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView, DetailView
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView
 from django.urls import reverse_lazy
 from .models import RightHandCar, OwnershipCalculation
 from .forms import CalculationForm
 
+# Функции
 def home(request):
     """Главная страница"""
     return render(request, 'calculator/home.html')
 
+def calculation_result(request):
+    """Результат расчета (заглушка)"""
+    context = {
+        'total_cost': 370000,
+        'breakdown': {
+            'customs': 90000,
+            'adaptation': 80000,
+            'insurance': 75000,
+            'fuel': 60000,
+            'maintenance': 50000,
+            'tax': 15000,
+        },
+        'per_year': 123333,
+        'price_rub': 300000,
+        'car': {
+            'brand': 'Toyota',
+            'model': 'Mark II',
+            'generation': 'JZX100',
+        },
+        'years': 3,
+        'mileage': 15000,
+        'region': 'Москва',
+    }
+    return render(request, 'calculator/result.html', context)
+
+# Классы
 class CarListView(ListView):
-    """Список доступных автомобилей"""
+    """Список автомобилей"""
     model = RightHandCar
     template_name = 'calculator/car_list.html'
     context_object_name = 'cars'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cars = context['cars']
+        
+        # Группируем по маркам
+        cars_by_brand = {}
+        for car in cars:
+            brand_name = car.get_brand_display()
+            if brand_name not in cars_by_brand:
+                cars_by_brand[brand_name] = []
+            cars_by_brand[brand_name].append(car)
+        
+        context['cars_by_brand'] = cars_by_brand
+        context['total_cars'] = cars.count()
+        return context
 
 class CalculationCreateView(CreateView):
-    """Создание нового расчета"""
+    """Форма расчета"""
     model = OwnershipCalculation
     form_class = CalculationForm
     template_name = 'calculator/calculation_form.html'
     success_url = reverse_lazy('calculation_result')
-    
-    def form_valid(self, form):
-        # Если пользователь авторизован, сохраняем его
-        if self.request.user.is_authenticated:
-            form.instance.user = self.request.user
-        return super().form_valid(form)
-
-def calculation_result(request, pk=None):
-    """Результат расчета"""
-    # Пока заглушка
-    context = {
-        'message': 'Здесь будет результат расчета',
-        'total_cost': 150000,
-        'years': 3,
-    }
-    return render(request, 'calculator/result.html', context)
